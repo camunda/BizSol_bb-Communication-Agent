61e413f22254eb620622d01cbd8364596f285491
Volker Buzek <dev@metalodge.com>
2026-02-11 21:42:09 +0100

    refactor(ProcessVariables): simplify class by using Lombok annotations and removing boilerplate code

--- src/main/java/io/camunda/bizsol/bb/comm_agent/models/ProcessVariables.java
+++ src/main/java/io/camunda/bizsol/bb/comm_agent/models/ProcessVariables.java
@@ -1,63 +1,15 @@
 package io.camunda.bizsol.bb.comm_agent.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class ProcessVariables {
     private ObjectA a;
     private String concatenatedResult;
-
-    public ProcessVariables() {}
-
-    public ProcessVariables(ObjectA a, String concatenatedResult) {
-        this.a = a;
-        this.concatenatedResult = concatenatedResult;
-    }
-
-    public ObjectA getA() {
-        return a;
-    }
-
-    public void setA(ObjectA a) {
-        this.a = a;
-    }
-
-    public String getConcatenatedResult() {
-        return concatenatedResult;
-    }
-
-    public void setConcatenatedResult(String concatenatedResult) {
-        this.concatenatedResult = concatenatedResult;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private ObjectA a;
-        private String concatenatedResult;
-
-        public Builder a(ObjectA a) {
-            this.a = a;
-            return this;
-        }
-
-        public Builder concatenatedResult(String concatenatedResult) {
-            this.concatenatedResult = concatenatedResult;
-            return this;
-        }
-
-        public ProcessVariables build() {
-            return new ProcessVariables(a, concatenatedResult);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "ProcessVariables{"
-                + "a="
-                + a
-                + ", concatenatedResult='"
-                + concatenatedResult
-                + '\''
-                + '}';
-    }
 }
