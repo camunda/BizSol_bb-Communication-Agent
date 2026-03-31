a97f1a9cc7a1da1151369e1ec36cd78964596871
Jan Ladleif <jan.ladleif@camunda.com>
2026-03-31 11:15:35 +0200

    chore: remove service worker for 8.9 bb

--- src/main/java/io/camunda/bizsol/bb/comm_agent/models/CorrelationKey.java
+++
@@ -1,3 +0,0 @@
-package io.camunda.bizsol.bb.comm_agent.models;
-
-public record CorrelationKey(String value) {}
--- src/main/java/io/camunda/bizsol/bb/comm_agent/models/CustomCommunicationContext.java
+++
@@ -1,7 +0,0 @@
-package io.camunda.bizsol.bb.comm_agent.models;
-
-/**
- * Marker interface for custom communication context implementations beyond the built-in {@link
- * CommunicationContext} types.
- */
-public non-sealed interface CustomCommunicationContext extends CommunicationContext {}
